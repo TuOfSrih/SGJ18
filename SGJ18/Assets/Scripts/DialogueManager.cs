a6e65857740df4d1f8a0081ab17b95142681798f
KuroiX <julian.geheeb@gmx.de>
2018-06-15 23:36:08 +0200

    finished dialogue box

--- SGJ18/Assets/Scripts/DialogueManager.cs
+++ SGJ18/Assets/Scripts/DialogueManager.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+	public Text nameText;
+	public Text dialogueText;
 
+	public Animator animator;
+	
 	private Queue<string> sentences;
 	
 	void Start ()
@@ -14,14 +19,18 @@ public class DialogueManager : MonoBehaviour
 
 	public void StartDialogue(Dialogue dialogue)
 	{
-		Debug.Log("Starting conversation with " + dialogue.name);
+		animator.SetBool("IsOpen", true);
+		
+		nameText.text = dialogue.name;
 		
 		sentences.Clear();
 
-		foreach (string sentence in sentences)
+		foreach (string sentence in dialogue.sentences)
 		{
 			sentences.Enqueue(sentence);
 		}
+
+		DisplayNextSentence();
 	}
 
 	public void DisplayNextSentence()
@@ -33,11 +42,22 @@ public class DialogueManager : MonoBehaviour
 		}
 
 		string sentence = sentences.Dequeue();
-		
+		StopAllCoroutines();
+		StartCoroutine(TypeSentence(sentence));
+	}
+
+	IEnumerator TypeSentence(string sentence)
+	{
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return null;
+		}
 	}
 
 	void EndConversation()
 	{
-		Debug.Log("End of conv");
+		animator.SetBool("IsOpen", false);
 	}
 }
