33b7fd925e0b2c69f4b69fa298038a7e838d4f4e
KuroiX <julian.geheeb@gmx.de>
2018-06-15 22:46:00 +0200

    Script folder and started working on dialogue system

---
+++ SGJ18/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+
+	private Queue<string> sentences;
+	
+	void Start ()
+	{
+		sentences = new Queue<string>();
+	}
+
+	public void StartDialogue(Dialogue dialogue)
+	{
+		Debug.Log("Starting conversation with " + dialogue.name);
+		
+		sentences.Clear();
+
+		foreach (string sentence in sentences)
+		{
+			sentences.Enqueue(sentence);
+		}
+	}
+
+	public void DisplayNextSentence()
+	{
+		if (sentences.Count == 0)
+		{
+			EndConversation();
+			return;
+		}
+
+		string sentence = sentences.Dequeue();
+		
+	}
+
+	void EndConversation()
+	{
+		Debug.Log("End of conv");
+	}
+}
