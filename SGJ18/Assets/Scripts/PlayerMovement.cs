8af353eb810eac151612ff5fdf28b7f7812ab989
nav0t <realandnotfake@email.com>
2018-06-15 21:25:23 +0200

    Shitty Player Controller

---
+++ SGJ18/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float movementSpeed;
+	public float acceleration;
+
+	Rigidbody2D rigidbody;
+
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		Vector2 target = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * movementSpeed;
+		rigidbody.velocity = Vector2.MoveTowards(rigidbody.velocity, target, acceleration * Time.deltaTime);
+
+	}
+}
