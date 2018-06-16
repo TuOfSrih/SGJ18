99093b24f3652da8527ceea0d11d53bd95a4ab40
nav0t <realandnotfake@email.com>
2018-06-16 18:00:23 +0200

    Made the level again

--- SGJ18/Assets/Scripts/RayLight2D.cs
+++ SGJ18/Assets/Scripts/RayLight2D.cs
@@ -8,8 +8,6 @@ public class RayLight2D : Light2D {
 	public float rayStartOffset = 1.5f;
 	public int raysPerDeg = 2;
 
-	public LayerMask mask;
-
 	public override void Render(Material mat) {
 		base.Render(mat);
 		//Debug.Log("Rendering");
@@ -23,7 +21,9 @@ public class RayLight2D : Light2D {
 		for (int i = 0; i <= coneAngle * 2 * raysPerDeg; i++) {
 			float ang = angle + ((float)i / raysPerDeg - coneAngle) * Mathf.Deg2Rad;
 			Vector3 cast = new Vector2(Mathf.Cos(ang), Mathf.Sin(ang));
-			RaycastHit2D hit = Physics2D.Raycast(position + rayStartOffset * cast.normalized, cast, maxDistance - rayStartOffset, mask);
+
+			//-513 = alles - physicalnolight layer
+			RaycastHit2D hit = Physics2D.Raycast(position + rayStartOffset * cast.normalized, cast, maxDistance - rayStartOffset, -513);
 			if (hit.collider == null) {
 				vertices[i] = position + cast.normalized * maxDistance;
 			} else {
