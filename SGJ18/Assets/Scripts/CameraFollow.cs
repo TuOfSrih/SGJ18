0a6c0f2d57c7b46fbc2349d5bfef9d3590763dd8
nav0t <realandnotfake@email.com>
2018-06-16 06:19:04 +0200

    renamed CameraFollow script to avoid namespace issues

---
+++ SGJ18/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	public GameObject player;
+	public Vector3 offset = Vector3.back * 3;
+	public float speed = 30;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 target = player.transform.position + offset;
+		transform.position = Vector3.Lerp(transform.position, target, speed * Time.deltaTime);
+	}
+}
