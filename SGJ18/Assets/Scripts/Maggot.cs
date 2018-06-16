ba31c084df19bc7c6403029a06b5d73e10e9be73
nav0t <realandnotfake@email.com>
2018-06-16 23:49:21 +0200

    Maden Madness

---
+++ SGJ18/Assets/Scripts/Maggot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maggot : MonoBehaviour {
+
+	public float stompDist = .3f;
+	public Sprite stomped;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (PlayerMovement.instance != null) {
+			if (Vector2.Distance(transform.position, PlayerMovement.instance.transform.position) < stompDist) {
+				GetComponent<Animator>().enabled = false;
+				GetComponent<SpriteRenderer>().sprite = stomped;
+				Debug.Log("Crushed the MADENs");
+			}
+		}
+	}
+}
