f616be49ef7d9dac5de187d50950a806d3c9cc0b
KuroiX <julian.geheeb@gmx.de>
2018-06-16 16:29:06 +0200

    joycon controlls

--- SGJ18/Assets/Scripts/DialogueManager.cs
+++ SGJ18/Assets/Scripts/DialogueManager.cs
@@ -31,6 +31,7 @@ public class DialogueManager : MonoBehaviour
 		}
 
 		DisplayNextSentence();
+		
 	}
 
 	public void DisplayNextSentence()
--- SGJ18/Assets/Scripts/PlayerMovement.cs
+++ SGJ18/Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Jobs.LowLevel.Unsafe;
 using UnityEditor;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+	
+	private List<Joycon> joycons;
+
+	// Values made available via Unity
+	public float[] stick;
+	public Vector3 gyro;
+	public Vector3 accel;
+	public int jc_ind = 0;
+	public Quaternion orientation;
+	public Vector3 or;
+	
+	
+	
+	
+	
 
 	public static PlayerMovement instance;
 
@@ -15,8 +31,8 @@ public class PlayerMovement : MonoBehaviour {
 
 	private bool nearCloset;
 	private bool nearDiary;
-	private bool isReading;
-	
+	public bool isReading;
+	private float rad;
 	
 
 	public bool isHidden;
@@ -40,22 +56,75 @@ public class PlayerMovement : MonoBehaviour {
 		nearCloset = false;
 		nearDiary = false;
 		isHidden = false;
+		rad = 0.0f;
+		
+		
+		gyro = new Vector3(0, 0, 0);
+		accel = new Vector3(0, 0, 0);
+		// get the public Joycon array attached to the JoyconManager in scene
+		joycons = JoyconManager.Instance.j;
+		if (joycons.Count < jc_ind+1){
+			Destroy(gameObject);
+		}
 	}
 	
 	void Update () {
-		Vector2 target = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * movementSpeed;
-		rigidbody.velocity = Vector2.MoveTowards(rigidbody.velocity, target, acceleration * Time.deltaTime);
-		facing = (Vector2)UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position;
+		Joycon j = joycons [jc_ind];
+		if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
+		{
+			j.Recenter();
+		}
+
+		if (j.GetButton(Joycon.Button.SL))
+		{
+			rad -= Time.deltaTime * 200;
+		}
+
+		if (j.GetButton(Joycon.Button.SR))
+		{
+			rad += Time.deltaTime * 200;
+		}
+		if (!isReading)
+		{
+			if (j.isLeft)
+			{
+				Vector2 target = new Vector2(j.GetStick()[0], -j.GetStick()[1]) * movementSpeed;
+				rigidbody.velocity = Vector2.MoveTowards(rigidbody.velocity, target, acceleration * Time.deltaTime);
+				//facing = (Vector2) UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition) -
+				//(Vector2) transform.position;
+				accel = j.GetAccel();
+				facing = new Vector2(accel.x, -accel.y) * 2 + (Vector2) transform.position;
+
+				facing = new Vector2(Mathf.Sin(rad / 180 * Mathf.PI), Mathf.Cos(rad / 180 * Mathf.PI)) * 3 + (Vector2) transform.position;
+				Debug.DrawLine(facing, transform.position, Color.green);
+			}
+			else
+			{
+				Vector2 target = new Vector2(j.GetStick()[1], -j.GetStick()[0]) * movementSpeed;
+				rigidbody.velocity = Vector2.MoveTowards(rigidbody.velocity, target, acceleration * Time.deltaTime);
+				//facing = (Vector2) UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition) -
+				//(Vector2) transform.position;
+				accel = j.GetAccel();
+				facing = new Vector2(-accel.x, accel.y) * 2 + (Vector2) transform.position;
+				facing = new Vector2(Mathf.Sin(rad / 180 * Mathf.PI), Mathf.Cos(rad / 180 * Mathf.PI)) * 3 + (Vector2) transform.position;
+				Debug.DrawLine(facing, transform.position, Color.green);
+			}
+		}
+
 		if (rayLight != null) {
 			rayLight.position = transform.position;
 			rayLight.facing = facing;
 		}
 
-
+		
 		if (nearCloset)
 		{
-			if (Input.GetKeyDown(KeyCode.Space))
+			if (Input.GetKeyDown(KeyCode.Space) || j.GetButtonDown (Joycon.Button.DPAD_DOWN) 
+			                                    || j.GetButtonDown (Joycon.Button.DPAD_UP) 
+			                                    || j.GetButtonDown (Joycon.Button.DPAD_RIGHT) 
+			                                    || j.GetButtonDown (Joycon.Button.DPAD_LEFT))
 			{
+				rigidbody.velocity = Vector3.zero;
 				if (isHidden)
 				{
 					isHidden = false; 
@@ -74,8 +143,12 @@ public class PlayerMovement : MonoBehaviour {
 		
 		if (nearDiary)
 		{
-			if (Input.GetKeyDown(KeyCode.Space))
+			if (Input.GetKeyDown(KeyCode.Space) || j.GetButtonDown (Joycon.Button.DPAD_DOWN) 
+			                                    || j.GetButtonDown (Joycon.Button.DPAD_UP) 
+			                                    || j.GetButtonDown (Joycon.Button.DPAD_RIGHT) 
+			                                    || j.GetButtonDown (Joycon.Button.DPAD_LEFT))
 			{
+				rigidbody.velocity = Vector3.zero;
 				if (isReading)
 				{
 					dialogue.DisplayNextSentence();
@@ -106,7 +179,6 @@ public class PlayerMovement : MonoBehaviour {
 	
 	void OnTriggerExit2D(Collider2D coll)
 	{
-		Debug.Log("first");
 		if (coll.CompareTag("diary"))
 		{
 			nearDiary = false;
@@ -114,7 +186,6 @@ public class PlayerMovement : MonoBehaviour {
 
 		if (rigidbody.simulated && coll.CompareTag("closet"))
 		{
-			Debug.Log("second");
 			nearCloset = false;
 			closet = null;
 		}
