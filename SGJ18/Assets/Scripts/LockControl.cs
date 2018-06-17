46209ce050701f824d339b687373beac3bd36857
Unknown <f.herick@gmx.de>
2018-06-17 05:35:54 +0200

    Added Lockcontrol to lobby

---
+++ SGJ18/Assets/Scripts/LockControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockControl : MonoBehaviour {
+
+    public int unlocked = 0;
+
+    public SpriteRenderer first;
+    public SpriteRenderer second;
+    public SpriteRenderer third;
+
+    public Sprite unlock;
+	// Use this for initialization
+	public void increment() {
+        unlocked++;
+        first.sprite = unlock;
+        if (unlocked > 1) second.sprite = unlock;
+        if (unlocked > 2) third.sprite = unlock;
+
+    }
+}
